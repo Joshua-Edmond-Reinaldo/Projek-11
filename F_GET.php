056269fd1ed5c1ea732ef456432e33266c7b90f5
Joshua-Edmond-Reinaldo <112202407257@mhs.dinus.ac.id>
2025-12-07 17:17:17 +0700

    first commit

--- F_GET.php
+++ F_GET.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Data (GET)</title>
     <style>
-        /* --- MODERN PROGRAMMER THEME FORM STYLE --- */
         @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;500;600;700&display=swap');
 
         * {
